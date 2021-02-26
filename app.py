867178bd4ee30fc81346f9f1a8d767cbca423339
Ken Johnson <dragonkenkj@gmail.com>
2021-02-26 11:33:12 -0500

    Connected dashboard to data, still bugs with layout and graphs

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
+import math
+import pandas as pd
+import numpy as np
+import pypopulation
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 
 import plotly.graph_objects as go
+import plotly.express as px
 
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 colors = {
@@ -14,6 +20,9 @@ colors = {
     "black": "#000",
 }
 
+global_df = pd.read_csv("./data/_raw_data.csv")
+df = pd.read_csv("./data/Global_vaccinations.csv")
+
 app = dash.Dash(
     __name__,
     external_stylesheets=external_stylesheets,
@@ -22,6 +31,12 @@ app = dash.Dash(
 
 
 def navbar():
+    dropdown_options = [
+        {"label": country, "value": country} for country in set(global_df["country"])
+    ]
+    dropdown_options.sort(key=lambda x: x["label"])
+    global_option = {"label": "Global", "value": "Global"}
+    dropdown_options.insert(0, global_option)
     return html.Div(
         className="nav",
         children=[
@@ -29,7 +44,7 @@ def navbar():
             dcc.Dropdown(
                 className="dropdown",
                 id="region",
-                options=[{"label": "Global", "value": "Global"}],
+                options=dropdown_options,
                 value="Global",
             ),
         ],
@@ -37,20 +52,43 @@ def navbar():
 
 
 def percent_coutries():
-    countries = [
-        "Canada",
-        "Japan",
-        "USA",
-        "Italy",
-        "France",
-        "Germany",
-        "UK",
-        "China",
-        "Mexico",
-        "India",
-    ]
-    percentages = [50, 43, 40, 37, 33, 31, 29, 25, 21, 19]
-    fig = go.Figure(go.Bar(x=countries, y=percentages))
+    cur_df = pd.read_csv("./data/_raw_data.csv")
+    cur_df = cur_df.groupby(["country", "iso_code"], as_index=False)[
+        "daily_vaccinations"
+    ].sum()
+    cur_df["iso_code"].fillna("", inplace=True)
+    percentage = []
+    for i, row in cur_df.iterrows():
+        population = 0
+        # print(row["iso_code"])
+        if row["iso_code"] != "":
+            population = pypopulation.get_population(row["iso_code"])
+        # print(population)
+        if population:
+            print(population)
+            percentage.append(
+                np.asarray(
+                    [
+                        row["country"],
+                        row["daily_vaccinations"] / population,
+                    ]
+                )
+            )
+        else:
+            # pass
+            print(row["country"])
+    # print(percentage)
+    percentage = np.asarray(percentage)
+    percentage[percentage[:, 1].argsort()]
+    top_countries = percentage
+    print(top_countries)
+    fig = go.Figure(
+        go.Bar(x=top_countries[:, 1], y=top_countries[:, 0], orientation="h")
+    )
+    fig.update_layout(
+        title="Vaccination Progress Ranking",
+        margin=dict(t=30, l=10, b=5, r=10),
+    )
     return html.Div(
         className="percent-countries-container",
         children=[
@@ -63,17 +101,51 @@ def percent_coutries():
     )
 
 
-def top_stat(stat, heading):
+def vacc_velocity():
+    return html.Div()
+
+
+def top_stat(stat, heading, class_id):
     return html.Div(
         className="top-stat-container",
         children=[
             html.Div(
                 className="top-stats card",
                 children=[
-                    html.H2(className="stat", children=stat),
+                    html.H2(className="stat", id=class_id, children=stat),
                     html.H6(className="header", children=heading),
                 ],
             ),
+        ],
+    )
+
+
+def top_stat_graph(cur_df):
+    """
+    Draw sparkline.
+    """
+    df_tail = cur_df.tail(7)
+    fig = go.Figure(
+        go.Scatter(
+            x=np.arange(7),
+            y=df_tail["daily_vaccinations"],
+            marker=dict(size=1),
+            line=dict(color="#87ceeb"),
+        )
+    )
+    fig.update_xaxes(visible=False, fixedrange=True)
+    fig.update_yaxes(visible=False, fixedrange=True)
+    fig.update_layout(
+        height=45,
+        showlegend=False,
+        plot_bgcolor="white",
+        margin=dict(t=5, l=10, b=5, r=10),
+    )
+    return html.Div(
+        className="top-stats card",
+        children=[
+            dcc.Graph(figure=fig),
+            html.H6(className="header", children="Past 7 Days"),
         ],
     )
 
@@ -82,29 +154,32 @@ def top_stats():
     return html.Div(
         className="top-stats-container",
         children=[
-            top_stat("Feb. 15", "Latest Update"),
-            top_stat("33%", "Vaccinated"),
-            top_stat("60%", "Vaccination Intent"),
-            top_stat("1000", "Vaccinnated Today"),
-            top_stat("33%", "Vaccinnated"),
+            top_stat("Feb. 15", "Latest Update", "update_date"),
+            top_stat("33%", "Vaccinated", "vaccinated"),
+            top_stat("60%", "Vaccination Intent", "threshold"),
+            top_stat("1000", "Vaccinnated Today", "today"),
+            html.Div(
+                className="top-stat-container",
+                id="sparkline",
+                children=[
+                    top_stat_graph(df),
+                ],
+            ),
         ],
     )
 
 
-def pred_full_vacc():
-    dates = ["Jan 1st", "Jan 2nd"]
-    daily_vaccinated = [100, 200]
-    daily_cases = [300, 200]
+def pred_full_vacc(cur_df):
+    daily_vaccinated = cur_df["daily_vaccinations"].values
+    dates = cur_df["date"].values
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=dates, y=daily_vaccinated))
-    fig.add_trace(go.Scatter(x=dates, y=daily_cases))
     fig.update_layout(
         title="Predicted Fully Vaccinated Date",
         xaxis_title="Date",
         yaxis_title="Number of Vaccinations/Covid-19 Cases",
     )
     return html.Div(
-        className="pred-full-vacc-container",
         children=[
             dcc.Graph(
                 className="pred-full-vacc-graph card", id="pred-full-vacc", figure=fig
@@ -114,7 +189,17 @@ def pred_full_vacc():
 
 
 def right():
-    return html.Div(className="right", children=[top_stats(), pred_full_vacc()])
+    return html.Div(
+        className="right",
+        children=[
+            top_stats(),
+            html.Div(
+                className="pred-full-vacc-container",
+                id="pred",
+                children=[pred_full_vacc(df)],
+            ),
+        ],
+    )
 
 
 def homepage():
@@ -124,7 +209,7 @@ def homepage():
     )
 
 
-def countrypage():
+def countrypage(country):
     return html.Div(className="country-page", children=[vacc_velocity(), right()])
 
 
@@ -137,10 +222,24 @@ app.layout = html.Div(children=[navbar(), dashboard()])
 
 @app.callback(
     Output(component_id="dashboard", component_property="children"),
+    Output(component_id="update_date", component_property="children"),
+    Output(component_id="vaccinated", component_property="children"),
+    Output(component_id="threshold", component_property="children"),
+    Output(component_id="today", component_property="children"),
+    Output(component_id="sparkline", component_property="children"),
+    Output(component_id="pred", component_property="children"),
     Input(component_id="region", component_property="value"),
 )
 def change_page(country):
-    return homepage() if country == "global" else countrypage()
+    new_df = pd.read_csv(f"./data/{country}_vaccinations.csv")
+    page = homepage() if country == "Global" else countrypage(country)
+    update_date = new_df.iloc[[-1]]["date"].to_string(index=False)[5:]
+    vaccinated = 33
+    threshold = 70
+    today = float(new_df.iloc[[-1]]["daily_vaccinations"].to_string(index=False))
+    sparkline = top_stat_graph(new_df)
+    pred = pred_full_vacc(new_df)
+    return page, update_date, vaccinated, threshold, today, sparkline, pred
 
 
 if __name__ == "__main__":
